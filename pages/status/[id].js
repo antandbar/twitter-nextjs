bc2b4cf476139be1e66f3e6cdbdd49d2e3bf939a
Antonio Andreu <antonio.andreu.barreiro@gmail.com>
2022-08-17 11:52:35 +0200

    feat: Se implementa getServerSideProps

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -1,15 +1,29 @@
-import Devit from "../../components/devit";
+import Devit from '../../components/devit';
 
 const DavitPage = props => {
   console.log(props);
-  return (<>
-  <Devit {...props}/>
-  </>)
+  return (
+    <>
+      <Devit {...props} />
+    </>
+  );
 };
 
 export default DavitPage;
 
-DavitPage.getInitialProps = context => {
+export const getServerSideProps = async context => {
+  const { params, res } = context;
+  const { id } = params;
+  const apiResponse = await fetch(`http://localhost:3000/api/devits/${id}`);
+
+  if (apiResponse.ok) {
+    const props = await apiResponse.json();
+    return { props };
+  }
+  if (res) res.writeHead(301, { Location: '/home' }).end();
+};
+
+/* DavitPage.getInitialProps = context => {
   const { query, res } = context;
   const { id } = query;
   return fetch(`http://localhost:3000/api/devits/${id}`).then(apiResponse => {
@@ -17,4 +31,4 @@ DavitPage.getInitialProps = context => {
     if (res) res.writeHead(301, {Location: '/home'}).end();
   
   });
-};
+}; */
